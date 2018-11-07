2323f86f55fd564fee313e8a4779d63e51b63072
Kelit <kataevdenis@mail.ru>
2018-11-07 11:48:54 +0700

    Add
    1)Add img,css,html file
    2)Add the forms for user validation
    3)Add in views def user_login

--- app/views.py
+++ app/views.py
@@ -1,7 +1,19 @@
 from app import app
+from flask import render_template, flash
+from flask import redirect
 
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template("index.html")
+
+@app.route('/login', methods=['GET','POST'])
+def login_user():
+    form = LoginForm
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
