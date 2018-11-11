07e7553244cf8d0c8db837574410a909e9219d83
Kelit <kataevdenis@mail.ru>
2018-11-11 17:28:13 +0700

    Add
    1)correction form.py
    2)add icon
    3)add dbout_me.html
    3)correction

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember_me')
-    submit = SubmitField('Sign In')
+    username = StringField('Пользователь', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Вход')
--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ from app.forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html")
+    return render_template("index.html",active1="active")
 
 @app.route('/login', methods=['GET', 'POST'])
 def login_user():
@@ -16,4 +16,8 @@ def login_user():
         flash('Login requested for user {}, remember_me'.format(
             form.username.data, form.remember_me.data))
         return redirect('/index')
-    return render_template('login.html', title='Регистрация', form=form)
+    return render_template('login.html',active4="active", title='Регистрация', form=form)
+
+@app.route('/about')
+def about():
+    return render_template("about_me.html",active3="active", title='Об авторе')
