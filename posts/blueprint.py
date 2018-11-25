8f7fb3d2ec95b3bdfd4bae75c9933541fa4f2c8e
Kelit <nobilismendaxf@protonmail.ch>
2018-11-25 22:35:28 +0700

    Add
    1) add Tag templates
    2) add method tag_details

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask import render_template
-from app.models import Post
+from app.models import Post, Tag
 from flask_login import login_required
 
 posts = Blueprint('posts',__name__,template_folder='templates')
@@ -12,9 +12,18 @@ def index():
     posts = Post.query.all()
     return render_template('posts/list.html', posts=posts)
 
-
+# /blog/post
 @posts.route('/<slug>')
 @login_required
 def post_detail(slug):
     post = Post.query.filter(Post.slug==slug).first()
-    return render_template('posts/post_detail.html', post=post)
+    tags = post.tags
+    return render_template('posts/post_detail.html', post=post, tags=tags)
+
+#/blog/tag/code
+@posts.route('/tag/<slug>')
+@login_required
+def tag_detail(slug):
+    tag = Tag.query.filter(Tag.slug==slug).first()
+    posts = tag.posts.all()
+    return render_template('posts/tag_detail.html', tag=tag, posts=posts)
