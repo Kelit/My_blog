97eca45795db8157ba8fe6e76bab02dda5fdb194
Kelit <nobilismendaxf@protonmail.ch>
2018-11-25 22:06:31 +0700

    Add
    Add Table 'Tag' and 'post_tags'

--- app/models.py
+++ app/models.py
@@ -9,6 +9,11 @@ import re
 def slugify(s):
     pattern = r'[^\w+]'
     return re.sub(pattern, '-', s)
+
+post_tags = db.Table('post_tags',
+                     db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+                     db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+                     )
 #UserMixin,
 class User(UserMixin,db.Model): #USer
     id = db.Column(db.Integer,  primary_key=True)
@@ -37,12 +42,27 @@ class Post(db.Model):
         super(Post, self).__init__(*args, **kwargs)
         self.generate_slug()
 
+    tags = db.relationship('Tag', secondary=post_tags, backref=db.backref('posts', lazy='dynamic'))
+
     def generate_slug(self):
         if self.title:
             self.slug = slugify(self.title)
 
     def __repr__(self):
         return '<Post id: {}, title: {}>'.format(self.id, self.title)
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    slug = db.Column(db.String(100))
+
+    def __init__(self, *args, **kwargs):
+        super(Tag, self).__init__(*args,**kwargs)
+        self.slug = slugify(self.name)
+
+    def __repr__(self):
+        return '<Tag id: {}, name{}'.format(self.id, self.name)
 
 
 @login.user_loader
